9ba232b7c020cd8633c064c604c7c5e3d6d93bc5
Joyce Yang <jcyang@hmc.edu>
2017-10-11 09:35:33 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Enter the month of birth: ");
+        System.out.print("Enter day of birth: ");
+
+
+
+    }
+}
